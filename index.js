318ce1a0d60e6ce651e0d3894e9c8f1f27b182cc
mariegberg <mari36ix@stud.kea.dk>
2022-12-08 14:24:25 +0100

    Navbar almost

--- index.js
+++ index.js
@@ -7,3 +7,13 @@ function myFunction() {
       x.className = "topnav";
     }
   }
+
+  /* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
+/* function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+} */
