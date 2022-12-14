9fa9cf40c1a3e2629bf81c53cbe5dcfb252bba4f
mariegberg <mari36ix@stud.kea.dk>
2022-12-14 18:08:35 +0100

    More

--- index.js
+++ index.js
@@ -1,21 +1,12 @@
-/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
-  }
+document.addEventListener("DOMContentLoaded", init)
 
+function init() {
+  document.querySelector(".burger_menu").addEventListener("click", toggleBurgerMenu)
 
+}
 
-  /* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
-/* function myFunction() {
-  var x = document.getElementById("myLinks");
-  if (x.style.display === "block") {
-    x.style.display = "none";
-  } else {
-    x.style.display = "block";
-  }
-} */
+function toggleBurgerMenu() {
+  console.log(document.querySelector(".burger_container"))
+  //  toggle a class to the burger container that changes display:none to display:block or flex or grid
+  document.querySelector(".burger_container").classList.toggle("show")
+}
