910bfd9d5a06a41907ce4ca97a24d63424c76725
mariegberg <mari36ix@stud.kea.dk>
2022-12-15 16:19:33 +0100

    shit works

--- index.js
+++ index.js
@@ -10,3 +10,23 @@ function toggleBurgerMenu() {
   //  toggle a class to the burger container that changes display:none to display:block or flex or grid
   document.querySelector(".burger_container").classList.toggle("show")
 }
+
+
+/* FAQ SECTION */
+
+var faq = document.getElementsByClassName("faq-page");
+var i;
+for (i = 0; i < faq.length; i++) {
+    faq[i].addEventListener("click", function () {
+        /* Toggle between adding and removing the "active" class,
+        to highlight the button that controls the panel */
+        this.classList.toggle("active");
+        /* Toggle between hiding and showing the active panel */
+        var body = this.nextElementSibling;
+        if (body.style.display === "block") {
+            body.style.display = "none";
+        } else {
+            body.style.display = "block";
+        }
+    });
+}
